ce47774bdfacd7e2b1bbb4fd1cb9f9e95b6f206b
yihwan <yihwankim@gmail.com>
2018-08-21 15:27:48 -0700

    fix sample queries

--- frontend/components/main/form/form.jsx
+++ frontend/components/main/form/form.jsx
@@ -24,7 +24,7 @@ class Form extends React.Component {
             <li><a href="api/v1/facilities?name=Texaco&fine_min=1000"> api/v1/facilities?name=Texaco&fine_min=1000</a></li>
             <li><a href="api/v1/facilities?state=CA"> api/v1/facilities?state=CA</a></li>
             <li><a href="api/v1/facilities?inspection_count_min=10&inspection_count_max=15"> api/v1/facilities?penalty_count_min=10&penalty_count_max=15</a></li>
-            <li><a href="api/v1/facilities?fine_min=10?&fine_max=15"> api/v1/facilities?penalty_count_min=10&penalty_count_max=15</a></li>
+            <li><a href="api/v1/facilities?fines_min=1000?&fines_max=10000"> api/v1/facilities?fines_min=1000?&fines_max=10000</a></li>
 
           <br/>
           View everything:
