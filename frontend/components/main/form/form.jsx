36bc695639da3950a28fc0c03b05c870fa89b6b2
yihwan <yihwankim@gmail.com>
2018-08-21 15:40:59 -0700

    fix typos

--- frontend/components/main/form/form.jsx
+++ frontend/components/main/form/form.jsx
@@ -21,9 +21,9 @@ class Form extends React.Component {
             <li><a href="api/v1/enforcements/summarize"> api/v1/enforcements/summarize</a></li>
           <br/>
           Search and filter things:
-            <li><a href="api/v1/facilities?name=Texaco&fine_min=1000"> api/v1/facilities?name=Texaco&fine_min=1000</a></li>
+            <li><a href="api/v1/facilities?name=Texaco"> api/v1/facilities?name=Texaco</a></li>
             <li><a href="api/v1/facilities?state=CA"> api/v1/facilities?state=CA</a></li>
-            <li><a href="api/v1/facilities?inspection_count_min=10&inspection_count_max=15"> api/v1/facilities?penalty_count_min=10&penalty_count_max=15</a></li>
+            <li><a href="api/v1/facilities?inspection_count_min=10&inspection_count_max=15"> api/v1/facilities?inspection_count_min=10&inspection_count_max=15</a></li>
             <li><a href="api/v1/facilities?fines_min=1000&fines_max=10000"> api/v1/facilities?fines_min=1000&fines_max=10000</a></li>
 
           <br/>
