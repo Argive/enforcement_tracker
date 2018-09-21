34e7dc3c3c040873cb98f1a9422c3480accdc784
yihwan <yihwankim@gmail.com>
2018-09-21 10:20:21 -0700

    update Inspections/summarize

--- app/controllers/api/v1/inspections_controller.rb
+++ app/controllers/api/v1/inspections_controller.rb
@@ -2,8 +2,14 @@ class Api::V1::InspectionsController < ApplicationController
 
   def summarize
     @inspection_count = Facility.sum(:fac_inspection_count) # from past 5 years only
+    facilities_inspected = Facility.where('fac_inspection_count > ?', 0)
+    @facilities_inspected_count = facilities_inspected.count
+    @small_business_inspected_count = facilities_inspected.where(fac_type: 'small').count
+
     render json: {
-      inspection_count_5_yr: @inspection_count
+      inspection_count_5_yr: @inspection_count,
+      facilities_inspected_count: @facilities_inspected_count,
+      small_business_inspected_count: @small_business_inspected_count
     }
   end
 
